ff2bd30e1f193aed443e63c4f579e3c889cd1c55
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-03-17 12:29:26 -0700

    Add more sync variation methods
    Added a method to sync all entries, and a method sync only autosyncable entries. This allows one-off sync calls instead of continuously polling syncs.

--- lib/stagehand/staging/synchronizer.rb
+++ lib/stagehand/staging/synchronizer.rb
@@ -4,19 +4,28 @@ module Stagehand
       extend self
 
       def auto_sync(delay = 5.seconds)
-        scope = Configuration.ghost_mode ? CommitEntry.limit(1000) : CommitEntry.auto_syncable.limit(1000)
+        scope = autosyncable_entries.limit(1000)
 
         loop do
-          count = sync_entries(scope.reload)
-          puts "Synced #{count} entries"
+          puts "Synced #{sync_entries(scope.reload)} entries"
           sleep(delay) if delay
         end
       end
 
+      def sync_autosyncable
+        sync_entries(autosyncable_entries.find_each)
+      end
+
+      def sync_all
+        sync_entries(CommitEntry.find_each)
+      end
+
       # Copies all the affected records from the staging database to the production database
       def sync_record(record)
-        return Synchronizer.sync_entries(Checklist.new(record).compacted_entries)
+        sync_entries(Checklist.new(record).compacted_entries)
       end
+
+      private
 
       def sync_entries(entries)
         ActiveRecord::Base.transaction do
@@ -32,6 +41,10 @@ module Stagehand
         end
 
         return entries.length
+      end
+
+      def autosyncable_entries
+        Configuration.ghost_mode ? CommitEntry : CommitEntry.auto_syncable
       end
     end
   end
