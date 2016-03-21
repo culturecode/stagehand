b5ee1025b8dbf6fab70467a589e1893ed495d946
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-03-21 09:02:52 -0700

    Content operations should only include database modifying entries

--- lib/stagehand/staging/commit_entry.rb
+++ lib/stagehand/staging/commit_entry.rb
@@ -14,7 +14,7 @@ module Stagehand
       scope :start_operations,   lambda { where(:operation => START_OPERATION) }
       scope :end_operations,     lambda { where(:operation => END_OPERATION) }
       scope :control_operations, lambda { where(:operation => [START_OPERATION, END_OPERATION]) }
-      scope :content_operations, lambda { where.not(:record_id => nil, :table_name => nil) }
+      scope :content_operations, lambda { where(:operation => [INSERT_OPERATION, UPDATE_OPERATION, DELETE_OPERATION]) }
       scope :save_operations,    lambda { where(:operation => [INSERT_OPERATION, UPDATE_OPERATION]) }
       scope :delete_operations,  lambda { where(:operation => DELETE_OPERATION) }
       scope :uncontained,        lambda { where(:commit_id => nil) }
