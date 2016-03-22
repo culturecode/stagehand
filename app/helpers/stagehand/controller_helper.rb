b5c53b2d35d6c2c43f5dcdada1c3f135f2159245
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-03-22 14:53:50 -0700

    move capture and sync helper methods to Staging::Controller

--- app/helpers/stagehand/controller_helper.rb
+++
@@ -1,8 +0,0 @@
-module Stagehand
-  module ControllerHelper
-    # Creates a stagehand commit to log database changes associated with the given record
-    def commit_staging_changes(subject_record = nil, &block)
-      Staging::Commit.capture(subject_record, &block)
-    end
-  end
-end
--- lib/stagehand/staging/controller.rb
+++ lib/stagehand/staging/controller.rb
@@ -8,6 +8,16 @@ module Stagehand
         prepend_around_action :use_staging_database
       end
 
+      # Creates a stagehand commit to log database changes associated with the given record
+      def capture_changes(subject_record = nil, &block)
+        Staging::Commit.capture(subject_record, &block)
+      end
+
+      # Syncs the given record and all affected records to the production database
+      def sync_record(record)
+        Stagehand::Staging::Synchronizer.sync_record(record)
+      end
+
       private
 
       def use_staging_database(&block)
