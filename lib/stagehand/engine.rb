142688bbc8e43307082f092500c7604c38f7e85f
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-04-01 16:02:53 -0700

    Database has gotten big enough to be in its own file.

--- lib/stagehand/engine.rb
+++ lib/stagehand/engine.rb
@@ -9,11 +9,12 @@ module Stagehand
     # These require the rails application to be intialized because configuration variables are used
     initializer "stagehand.load_modules" do
       require "stagehand/configuration"
+      require "stagehand/database"
       require "stagehand/controller_extensions"
-      require "stagehand/helpers"
       require "stagehand/staging"
       require "stagehand/schema"
       require "stagehand/production"
+      require "stagehand/helpers"
     end
   end
 end
