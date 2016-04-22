1881beccee871c69fedebb0ea9b37562374ea420
Ryan Wallace <rywall@gmail.com>
2016-04-22 14:37:51 -0400

    Add ability to check whether a record is synced.

--- lib/stagehand/active_record_extensions.rb
+++ lib/stagehand/active_record_extensions.rb
@@ -1,5 +1,5 @@
 ActiveRecord::Base.class_eval do
-  # SYNC CALLBACK
+  # SYNC CALLBACKS
   define_callbacks :sync
 
   def self.before_sync(method, options = {})
@@ -8,5 +8,21 @@ ActiveRecord::Base.class_eval do
 
   def self.after_sync(method, options = {})
     set_callback :sync, :after, method, options
+  end
+
+  # SYNC STATUS
+  def self.inherited(subclass)
+    super
+
+    subclass.class_eval do
+      has_many :stagehand_commit_entries,
+        lambda { where(:stagehand_commit_entries => {:table_name => subclass.table_name}) },
+        :class_name => Stagehand::Staging::CommitEntry,
+        :foreign_key => :record_id
+
+      def stagehand_synced?
+        stagehand_commit_entries.blank?
+      end
+    end
   end
 end
