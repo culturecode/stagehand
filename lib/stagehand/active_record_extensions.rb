efb821ca02ff035a180fc578090ff8d1cb11b54c
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2018-02-05 12:37:13 -0800

    Threadsafe update
    This is a bit of a WIP but it allows stagehand to operate on Webbrick without `Rack::Lock`. The changes are geared towards ensuring that multi-threaded serving of requests does not cause the connection to be lost while connection swapping occurs in separate threads. The solution appears to be not to call `establish_connection` except during initialization. `establish_connection` removes the old connection pool, which causes a simultaneously executing thread to choke as it looks for that connection pool. Instead, we change the variable that contains the name of the connection pool we’re looking up. This requries some gymnastics, because while Rails 5 allows the name to change dynamically, it isn’t expecting separate threads to want the same `ActiveRecord` class to use separate connections.

--- lib/stagehand/active_record_extensions.rb
+++ lib/stagehand/active_record_extensions.rb
@@ -24,4 +24,32 @@ ActiveRecord::Base.class_eval do
     @has_stagehand = Stagehand::Schema.has_stagehand?(table_name) unless defined?(@has_stagehand)
     return @has_stagehand
   end
+
+  # MULTITHREADED CONNECTION HANDLING
+
+  # The original implementation of remove_connection uses @connection_specification_name, which is shared across Threads.
+  # We have overridden writes to that variable so they are stored in Thread.current, but we need to swap it in when a
+  # connection is removed.
+  def self.remove_connection(name = nil)
+    old = @connection_specification_name
+    @connection_specification_name = connection_specification_name
+    super
+  ensure
+    @connection_specification_name = old
+  end
+
+  def self.connection_specification_name=(connection_name)
+    load_stagehand_connection_specification_name
+    ActiveRecord::Base.logger.debug "setting connection name #{Thread.current.object_id} #{Thread.current['Stagehand:connection_specification_name'][self.name]} => #{connection_name}"
+    Thread.current['Stagehand:connection_specification_name'][self.name] = connection_name
+  end
+
+  def self.connection_specification_name
+    load_stagehand_connection_specification_name
+    Thread.current['Stagehand:connection_specification_name'][self.name] || super
+  end
+
+  def self.load_stagehand_connection_specification_name
+    Thread.current['Stagehand:connection_specification_name'] ||= {}
+  end
 end
--- lib/stagehand/database.rb
+++ lib/stagehand/database.rb
@@ -1,8 +1,29 @@
+require 'thread'
+
 module Stagehand
   module Database
     extend self
 
-    @@connection_name_stack = [Rails.env.to_sym]
+    # Threadsafe tracking of the connection stack
+    module ConnectionStack
+      @@connection_name_stack = Hash.new { |h,k| h[k] = [ Rails.env.to_sym ] }
+
+      def self.push(connection_name)
+        current_stack.push connection_name
+      end
+
+      def self.pop
+        current_stack.pop
+      end
+
+      def self.last
+        current_stack.last
+      end
+
+      def self.current_stack
+        @@connection_name_stack[Thread.current.object_id]
+      end
+    end
 
     def each(&block)
       with_production_connection(&block) unless Configuration.single_connection?
@@ -50,23 +71,27 @@ module Stagehand
     end
 
     def with_connection(connection_name)
-      different = current_connection_name != connection_name.to_sym
+      # @@semaphore.synchronize do
+        begin
+          different = current_connection_name != connection_name.to_sym
 
-      if different
-        @@connection_name_stack.push(connection_name.to_sym)
-        Rails.logger.debug "Connecting to #{current_connection_name}"
-        connect_to(current_connection_name)
-      else
-        Rails.logger.debug "Already connected to #{connection_name}"
-      end
+          if different
+            ConnectionStack.push(connection_name.to_sym)
+            Rails.logger.debug "Connecting to #{current_connection_name}"
+            connect_to(current_connection_name)
+          else
+            Rails.logger.debug "Already connected to #{connection_name}"
+          end
 
-      yield connection_name
-    ensure
-      if different
-        @@connection_name_stack.pop
-        Rails.logger.debug "Restoring connection to #{current_connection_name}"
-        connect_to(current_connection_name)
-      end
+          yield connection_name
+        ensure
+          if different
+            ConnectionStack.pop
+            Rails.logger.debug "Restoring connection to #{current_connection_name}"
+            connect_to(current_connection_name)
+          end
+        end
+      # end
     end
 
     def transaction
@@ -85,11 +110,11 @@ module Stagehand
     private
 
     def connect_to(connection_name)
-      ActiveRecord::Base.establish_connection(connection_name)
+      ActiveRecord::Base.connection_specification_name = connection_name
     end
 
     def current_connection_name
-      @@connection_name_stack.last
+      ConnectionStack.last
     end
 
     def database_name(connection_name)
@@ -106,7 +131,12 @@ module Stagehand
       self.abstract_class = true
 
       def self.init_connection
-        establish_connection(Configuration.staging_connection_name) unless Configuration.single_connection?
+        establish_connection(name)
+      end
+
+      # Ensure the connection pool is named after the desired connection, not "StagingProbe"
+      def self.name
+        Configuration.staging_connection_name
       end
 
       init_connection
@@ -116,7 +146,12 @@ module Stagehand
       self.abstract_class = true
 
       def self.init_connection
-        establish_connection(Configuration.production_connection_name) unless Configuration.single_connection?
+        establish_connection(name)
+      end
+
+      # Ensure the connection pool is named after the desired connection, not "ProductionProbe"
+      def self.name
+        Configuration.production_connection_name
       end
 
       init_connection
